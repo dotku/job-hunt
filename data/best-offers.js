7df9b364d4e4a38e8979215aca29cec704c30877
Weijing Jay Lin <weijingjaylin@gmail.com>
2020-03-09 17:39:00 -0700

    Create best-offers.js

---
+++ data/best-offers.js
@@ -0,0 +1,15 @@
+// https://www.forbes.com/sites/jeffkauflin/2017/11/14/the-20-best-company-benefits-and-11-companies-that-offer-them/#4e24206f47f1
+
+export.modules = [
+{name: "Gold’s Gym", "offers": 11},
+{name: "IBM", "offers": 11},
+{name: "L’Oreal", "offers": 6},
+{name: "Amazon", "offers": 6},
+{name: "Aetna", "offers": 6},
+{name: "UNICEF", "offers": 5},
+{name: "Experian", "offers": 5},
+{name: "Microsoft", "offers": 5},
+{name: "Massage Envy", "offers": 5},
+{name: "JPMorgan Chase", "offers": 5},
+{name: "Hilton", "offers": 5},
+]
